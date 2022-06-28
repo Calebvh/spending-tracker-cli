67ada84b7b0541bf19128abbb308546276b2205f
calebvh <caleb.vanhaaren@pe-ltd.com>
2022-06-27 22:27:50 -0400

    Now Parsing rows and columns

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use crate::chrono::{DateTime};
+use crate::chrono::format::ParseError;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,6 +12,18 @@ fn main() {
 
     let contents = fs::read_to_string(&args[1])
         .expect("Something went wrong reading the file");
+    let rows = contents.split("\n");
+    let mut i = 0;
+    for line in rows{
+        if i > 5{
+            break;
+        }
+        let cols = contents.split(",");
+        i+=1;
+        for item in cols{
+            println!("{}", item);
+        }
+    }
 
-    println!("With text:\n{}", contents);
+    //println!("With text:\n{}", contentsArr[0]);
 }
