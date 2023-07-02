b0bbfd139ca29df6f78c527dcbdeaf0d6ff83d7a
Caleb van Haaren <calebvanhaaren@gmail.com>
2023-07-01 22:59:10 -0400

    Starting CLI loop

--- src/main.go
+++ src/main.go
@@ -1,6 +1,39 @@
 package main
 
-func main() {
-	//CLI to run the program and prompt for file locations
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+
+	for {
+		showMenu()
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter your choice: ")
+		choice, _ := reader.ReadString('\n')
+		choice = strings.TrimSpace(choice)
+
+		// switch choice {
+		// case "1":
+		// 	addTransaction(&transactions)
+		// case "2":
+		// 	listTransactions(transactions)
+		// case "3":
+		// 	fmt.Println("Exiting program...")
+		// 	return
+		// default:
+		// 	fmt.Println("Invalid choice. Please try again.")
+		// }
+	}
+}
+
+func showMenu() {
+	fmt.Println("---- Transaction Tracker ----")
+	fmt.Println("1. Add Transaction")
+	fmt.Println("2. List Transactions")
+	fmt.Println("3. Exit")
 }
